5af549877a154b8a8ca63ddea9c0d34259f50534
Welcome Sithole <sitholewb@gmail.com>
2021-09-26 22:04:49 +0200

    Add project files.

---
+++ Dotnet.List.Pagination/Pagination.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dotnet.List.Pagination
+{
+	public class Pagination<T>
+	{
+		public Pagination(IList<T> results, long totalItems, int page = 1, int limit = 10)
+		{
+
+			var startIndex = (page - 1) * limit;
+			var endIndex = page * limit;
+			var totalPages = (int)Math.Ceiling((decimal)totalItems / (decimal)limit);
+
+			TotalItems = totalItems;
+			CurrentPage = page;
+			Results = results ?? new List<T>();
+
+			if (startIndex > 0)
+			{
+				PreviousPage = page - 1;
+			}
+			if (endIndex < totalItems)
+			{
+				NextPage = page + 1;
+			}
+
+			Pages = totalPages > 0 ? Enumerable.Range(1, totalPages).ToList() : new List<int>();
+		}
+
+		public long TotalItems { get; private set; }
+		public int CurrentPage { get; private set; }
+		public int? NextPage { get; private set; }
+		public int? PreviousPage { get; private set; }
+		public IList<int> Pages { get; private set; }
+		public IList<T> Results { get; private set; }
+	}
+
+
+}
---
+++ Dotnet.List.Pagination/PaginationExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Dotnet.List.Pagination
+{
+	public static class PaginationExtensions
+	{
+		public static async Task<Pagination<TSource>> AsPaginationAsync<TSource>(this IQueryable<TSource> source, int page, int limit, string sortColumn = null, bool orderByDescending = false)
+		{
+			var totalItems = await source.CountAsync();
+			if (!string.IsNullOrEmpty(sortColumn))
+			{
+				source = orderByDescending ? source.OrderByDescending(p => EF.Property<object>(p, sortColumn)) : source.OrderBy(p => EF.Property<object>(p, sortColumn));
+			}
+			var results = source.Skip((page - 1) * limit).Take(limit);
+
+			return new Pagination<TSource>(await results.ToListAsync(), totalItems, page, limit);
+
+		}
+
+		public static async Task<Pagination<TSource>> AsPaginationAsync<TSource>(this DbSet<TSource> source, int page, int limit, Expression<Func<TSource, bool>> expression, string sortColumn = null, bool orderByDescending = false) where TSource : class
+		{
+			var totalItems = await source.Where(expression).CountAsync();
+			var results = new List<TSource>();
+			if (!string.IsNullOrEmpty(sortColumn))
+			{
+				results = await (orderByDescending ? source.OrderByDescending(p => EF.Property<object>(p, sortColumn)) : source.OrderBy(p => EF.Property<object>(p, sortColumn))).ToListAsync();
+			}
+			else
+			{
+				results = await source.Where(expression).Skip((page - 1) * limit).Take(limit).ToListAsync();
+			}
+			return new Pagination<TSource>(results, totalItems, page, limit);
+
+		}
+	}
+}
