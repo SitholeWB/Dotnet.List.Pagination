f66b8c65f423a9b4fd6f8b72e71cf4e7e264aeba
Welcome Sithole <sitholewb@gmail.com>
2021-09-29 23:14:08 +0200

    PaginationAuto from Pagination Constructor

--- Pagination.Dotnet.List/PaginationAuto.cs
+++ Pagination.Dotnet.List/PaginationAuto.cs
@@ -6,16 +6,19 @@ namespace Pagination.Dotnet.List
 {
 	public class PaginationAuto<Tsource, Tdestination>
 	{
+		public PaginationAuto(Pagination<Tsource> pagination, Func<Tsource, Tdestination> convertTsourceToTdestinationMethod)
+		{
+			Results = pagination?.Results?.Select(a => convertTsourceToTdestinationMethod(a)) ?? Enumerable.Empty<Tdestination>();
+			TotalItems = pagination?.TotalItems ?? 0;
+			CurrentPage = pagination?.CurrentPage ?? 1;
+			NextPage = pagination?.NextPage;
+			Pages = pagination?.Pages ?? Enumerable.Empty<int>();
+		}
+
 		public PaginationAuto(IEnumerable<Tsource> results, long totalItems, Func<Tsource, Tdestination> convertTsourceToTdestinationMethod, int page = 1, int limit = 10)
 		{
-			if (limit <= 0)
-			{
-				throw new PaginationException("Limit must be greater than 0");
-			}
-			if (page <= 0)
-			{
-				throw new PaginationException("Page must be greater than 0");
-			}
+			ValidateInputs(page, limit);
+
 			var startIndex = (page - 1) * limit;
 			var endIndex = page * limit;
 			var totalPages = (int)Math.Ceiling((decimal)totalItems / (decimal)limit);
@@ -34,6 +37,18 @@ namespace Pagination.Dotnet.List
 			}
 
 			Pages = totalPages > 0 ? Enumerable.Range(1, totalPages) : Enumerable.Empty<int>();
+		}
+
+		private static void ValidateInputs(int page, int limit)
+		{
+			if (limit <= 0)
+			{
+				throw new PaginationException("Limit must be greater than 0");
+			}
+			if (page <= 0)
+			{
+				throw new PaginationException("Page must be greater than 0");
+			}
 		}
 
 		public long TotalItems { get; private set; }
